9df3133a0010b9add155eb206db65b24f799eaf2
68656c6c6f486f77417265596f75446f696e67 <51272629+68656c6c6f486f77417265596f75446f696e67@users.noreply.github.com>
2023-04-06 18:09:54 -0400

    init project

---
+++ PhysicEngine2D/src/main.cpp
@@ -0,0 +1,48 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main()
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(700, 600, "init", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glColor3f(1, 0, 0); // red
+		glVertex2d(0.0f, 0.5f);
+
+		glColor3f(0, 1, 0); // green
+		glVertex2d(-0.5f, -0.5f);
+
+		glColor3f(0, 0, 1); // blue
+		glVertex2d(0.5f, -0.5f);
+		glEnd();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
